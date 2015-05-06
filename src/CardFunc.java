aaf0a58f6b05d2be88048790843e61152fec79d2
Addison Zhang <zhangl3@rose-hulman.edu>
2015-05-06 15:32:09 -0400

    func94-pass

--- src/CardFunc.java
+++ src/CardFunc.java
@@ -1028,6 +1028,10 @@ public class CardFunc {
 	public void func89(){
 		myGame.currentPlayer.pLevel++;
 	}
+	
+	public void func94(){
+		func89();
+	}
 	/*
 	 * We need an end function that deals out the treasure cards, so that means that the player needs
 	 * to have a number of treasures to be dealt field.
