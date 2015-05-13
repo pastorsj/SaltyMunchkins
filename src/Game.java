a0dd003247ee76b5a24828b873250ebffad5b697
harrislb <lexibharris@yahoo.com>
2015-05-13 16:13:14 -0400

    update player at end of turn

--- src/Game.java
+++ src/Game.java
@@ -45,6 +45,7 @@ public class Game {
 			otherPlayer = p2;
 			turnPlayer = 1; 
 		}
+		this.mframe.mainPanel.bCardPanel.playerLabel.setText("p"+this.turnPlayer);
 		this.mframe.mainPanel.bCardPanel.playerLevel.setText("player level: " +this.currentPlayer.pLevel);
 		this.mframe.mainPanel.bCardPanel.playerCLevel.setText("combat level: " +this.currentPlayer.cLevel);
 		this.mframe.mainPanel.bCardPanel.monsterLevel.setText("monster level: " +this.mLevel);
