b0c715271c121bae33010009314e53119db380d3
Addison Zhang <zhangl3@rose-hulman.edu>
2015-05-13 15:04:53 -0400

    test c113-pass

--- src/InitializeCards.java
+++ src/InitializeCards.java
@@ -270,6 +270,10 @@ public class InitializeCards {
 		cardHash.get(112).pLevelBonus=1;
 		cardHash.get(112).armor=true;
 		cardHash.get(112).discard=false;
+		cardHash.get(113).numGold=600;
+		cardHash.get(113).pLevelBonus=3;
+		cardHash.get(113).footGear=true;
+		cardHash.get(113).discard=false;
 	}
 	
 	public void addConditionsForSelect() {
