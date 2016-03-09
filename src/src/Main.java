ddd4553d84c4bbbeed7554daab3f857d0192aeb8
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-03-09 18:20:45 -0500

    Another commit to test Travis CI

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,61 +1,55 @@
 package src;
+
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Random;
-
-import org.junit.Test;
-
-import static org.junit.Assert.*;
-
 
 public class Main {
-	
+
 	public static void main(String[] args) throws IOException {
 
 		initMain();
-		
-		
+
 	}
-	
-	public static void initMain() throws IOException{
-		
-		ReadFile file1 = new ReadFile(
-				"resources/CardList.txt");
+
+	public static void initMain() throws IOException {
+
+		ReadFile file1 = new ReadFile("resources/CardList.txt");
 		ArrayList<String> arrayOfLines = file1.OpenFile();
 		ArrayList<Integer> doorAv = new ArrayList<Integer>();
 		ArrayList<Integer> treasAv = new ArrayList<Integer>();
-	
+
 		doorAv.add(1);
 		doorAv.add(2);
 		doorAv.add(3);
-		doorAv.add(4); //not sure if this is for cards in play or for cards in hand?? assume hand
+		doorAv.add(4); // not sure if this is for cards in play or for cards in
+						// hand?? assume hand
 		doorAv.add(5);
-		//6
-		doorAv.add(7); //give away item of choice
+		// 6
+		doorAv.add(7); // give away item of choice
 		doorAv.add(8);
-		//9
+		// 9
 		doorAv.add(10);
-		//11
-		//12
-	//	doorAv.add(13);
-		//14
-	//	doorAv.add(15);
-		//16
-	//	doorAv.add(17);
-		//18
-		//19
-	//	doorAv.add(20);
-		//21
-		//22
+		// 11
+		// 12
+		// doorAv.add(13);
+		// 14
+		// doorAv.add(15);
+		// 16
+		// doorAv.add(17);
+		// 18
+		// 19
+		// doorAv.add(20);
+		// 21
+		// 22
 		doorAv.add(23);
-		doorAv.add(27); //level1 monster
+		doorAv.add(27); // level1 monster
 		doorAv.add(29);
-		
+
 		doorAv.add(30);
 		doorAv.add(42);
-		
+
 		treasAv.add(84);
-		//treasAv.add(85);
+		// treasAv.add(85);
 		treasAv.add(86);
 		treasAv.add(87);
 		treasAv.add(88);
@@ -87,35 +81,30 @@ public class Main {
 		treasAv.add(140);
 		treasAv.add(146);
 		treasAv.add(148);
-		
-		
-//		//double the treasures
-	
-		
 
-//		
-		InitializeCards ic = new InitializeCards(doorAv,treasAv);
-		
-		Game newGame = new Game(ic.doors,ic.treasure);
-		
+		// //double the treasures
+
+		//
+		InitializeCards ic = new InitializeCards(doorAv, treasAv);
+
+		Game newGame = new Game(ic.doors, ic.treasure);
+
 		newGame.shuffle(newGame.doors);
 		newGame.shuffle(newGame.treasures);
-		
+
 		ArrayList<Integer> hand = newGame.dealCards();
 		ArrayList<Card> deal;
-		newGame.p1.pHand=hand;
-		
-		newGame.p2.pHand=newGame.dealCards();
+		newGame.p1.pHand = hand;
+
+		newGame.p2.pHand = newGame.dealCards();
 		newGame.mframe = new MFrame(newGame);
 		newGame.mframe.setVisible(true);
 		newGame.mframe.setTitle("Munchkin");
-//	newGame.mframe.mainPanel.addBCard();
-	//	newGame.mframe.mainPanel.bCardPanel.etb.disable();
+		// newGame.mframe.mainPanel.addBCard();
+		// newGame.mframe.mainPanel.bCardPanel.etb.disable();
 		newGame.mframe.revalidate();
 		newGame.mframe.repaint();
-		
-		
+
 	}
-	
 
 }
