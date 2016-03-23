6bfade6f0db5bfd9802ea23802b6b6e57ac9f525
Trent Punt <tpunt3@gmail.com>
2016-03-23 10:39:36 -0400

    helping user clarity with menu bar/help

--- src/game/MFrame.java
+++ src/game/MFrame.java
@@ -2,12 +2,19 @@ package game;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 import gamepanels.MPanel;
 
@@ -34,10 +41,50 @@ public class MFrame extends JFrame {
 		this.add(this.mainPanel);
 		setTitle("Munchkin");
 		
+		createMenuBar();
 		
 		this.setVisible(true);
 		
+		JOptionPane.showMessageDialog(null,"Welcome to Munchkin Cthulhu! "
+				+ "This is a two player Munchkin game. For instructions on \n how to play, please "
+				+ "refer to the rules in the File menu. Otherwise, have fun!");
 		
+		
+	}
+
+	private void createMenuBar() {
+		
+		 JMenuBar menubar = new JMenuBar();
+
+	        JMenu file = new JMenu("File");
+	        file.setMnemonic(KeyEvent.VK_F);
+
+	        JMenuItem helpItem = new JMenuItem("Instructions");
+	        helpItem.setMnemonic(KeyEvent.VK_I);
+	        helpItem.setToolTipText("Instructions");
+	        helpItem.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent event) {
+	                
+	            }
+	        });
+	        
+	        file.add(helpItem);
+	        
+	        JMenuItem exitItem = new JMenuItem("Exit");
+	        exitItem.setMnemonic(KeyEvent.VK_E);
+	        exitItem.setToolTipText("Exit application");
+	        exitItem.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent event) {
+	                System.exit(0);
+	            }
+	        });
+
+	        file.add(exitItem);
+	        menubar.add(file);
+
+	        setJMenuBar(menubar);
 	}
 	
 	
