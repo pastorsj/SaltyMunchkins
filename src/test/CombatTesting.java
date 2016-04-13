c1f16037e89b9a907749569efe7f4b3c7f7a7633
Trent Punt <tpunt3@gmail.com>
2016-04-13 09:05:55 -0400

    combat Testing

--- src/test/CombatTesting.java
+++ src/test/CombatTesting.java
@@ -1,137 +1,85 @@
-//package test;
-//
-//import munchkin.game.Game;
-//import munchkin.game.InitializeCards;
-//import munchkin.game.MFrame;
-//import munchkin.game.buttons.ResolveConflictButton;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.util.ArrayList;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//
-//public class CombatTesting {
-//	public Game myGame;
-//	public InitializeCards init = new InitializeCards();
-//
-//	@Before
-//	public void setUp() {
-//
-//		myGame=new Game(init.doors,init.treasure);
-//		myGame.p1.pLevel=1;
-//		ArrayList<Integer> hand = new ArrayList<Integer>();
-//		hand.add(84); //plb = 4
-//		hand.add(88); //plb =2
-//		hand.add(87); //plb=1
-//		hand.add(86); // plb=2
-//		myGame.p1.pPlay=hand; //note this is NOT pHand, this is the cards in PLAY
-//		myGame.p1.cLevelCalculation();
-//		myGame.mframe = new MFrame(myGame);
-//		myGame.mframe.setVisible(false);
-//
-//
-//
-//
-//
-//
-//
-//	}
-//	@Test
-//	public void TestCLevelCalc() {
-//		assertEquals(10,myGame.p1.cLevel);
-//
-//	}
-//
-//	@Test
-//	public void TestMLevelBonusCalc(){
-//		myGame.p1.pPlay.add(29); //+10 to monster
-//		myGame.p1.pPlay.add(42); //-5 to monster
-//		assertEquals(5,myGame.p1.mLevelBonusCalculation());
-//	}
-//
-//	@Test
-//	public void TestDidIWin(){
-//		myGame.mLevel= 15;
-//		//p1 has player level bonus of 9
-//		myGame.p1.pPlay.add(42); //subtract 5 from monster.
-//		System.out.println("should be -5: " + init.cardHash.get(42).monsterLevelBonus);
-//		assertEquals(true, myGame.p1.didIwin(myGame));
-//		myGame.p2.pPlay.add(29);
-//		myGame.p2.pPlay.add(29);
-//		assertEquals(false, myGame.p2.didIwin(myGame));
-//	}
-//
-//	@Test
-//	public void TestGetTreasureForWinCard2() throws IOException{
-//		ArrayList<Integer> hand = new ArrayList<Integer>();
-//		ArrayList<Integer> handInPlay = new ArrayList<Integer>();
-//		ArrayList<Integer> hand2 = new ArrayList<Integer>();
-//		ResolveConflictButton diwb1 = new ResolveConflictButton("Did I Win?", myGame);
-//		handInPlay.add(2);
-//		handInPlay.add(88);
-//		myGame.p1.pHand=hand;
-//		myGame.p1.pPlay=handInPlay;
-//		myGame.p2.pPlay=hand2;
-//
-//		diwb1.didIWin();
-//
-//		assertEquals(1,myGame.p1.pHand.size());
-//		assertEquals(0,myGame.p1.pPlay.size());
-//
-//		//test that player level is set to 2
-//		assertEquals(2,myGame.p1.pLevel);
-//
-//	}
-//
-//	@Test
-//	public void TestCanBeat2AfterBeat1NoArmor(){
-//		ArrayList<Integer> hand = new ArrayList<Integer>();
-//		ArrayList<Integer> handInPlay = new ArrayList<Integer>();
-//		ArrayList<Integer> hand2 = new ArrayList<Integer>();
-//		ResolveConflictButton diwb1 = new ResolveConflictButton("Did I Win?", myGame);
-//		handInPlay.add(27); //level 1 monster
-//		myGame.p1.pHand=hand;
-//		myGame.p1.pPlay=handInPlay;
-//		myGame.p2.pPlay=hand2;
-//
-//		diwb1.didIWin();
-//			assertEquals(2,myGame.p1.pLevel);
-//
-//
-//		myGame.p1.pPlay.add(2); //level 2 monster
-//		myGame.updateMLevel(2);
-//		assertEquals(2,myGame.mLevel);
-//		diwb1.didIWin();
-//		assertEquals(3,myGame.p1.pLevel);
-//
-//	}
-//
-//	@Test
-//	public void TestCanBeat2AfterBeat1NoArmorNewWay(){
-//		ArrayList<Integer> hand = new ArrayList<Integer>();
-//		ArrayList<Integer> handInPlay = new ArrayList<Integer>();
-//		ArrayList<Integer> hand2 = new ArrayList<Integer>();
-//		ResolveConflictButton diwb1 = new ResolveConflictButton("Did I Win?", myGame);
-//		handInPlay.add(27); //level 1 monster
-//		myGame.playACard(27);
-//		myGame.p1.pHand=hand;
-//		myGame.p1.pPlay=handInPlay;
-//		myGame.p2.pPlay=hand2;
-//
-//		diwb1.didIWin();
-//			assertEquals(2,myGame.p1.pLevel);
-//
-//
-//		myGame.p1.pPlay.add(2); //level 2 monster
-//		myGame.playACard(2);
-//		diwb1.didIWin();
-//		assertEquals(3,myGame.p1.pLevel);
-//
-//	}
-//
-//
-//
-//}
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import munchkin.api.Player;
+import munchkin.api.Combat;
+import munchkin.cards.doors.AughostDerwraith;
+import munchkin.cards.doors.api.AbstractMonster;
+import munchkin.game.Game;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class CombatTesting {
+	
+	private Combat combat;
+
+	@Before
+	public void setUp() {
+		Game game = new Game(2);
+		combat = new Combat(game);
+	}
+	@Test
+	public void testAddMonster() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		AbstractMonster m1 = new AughostDerwraith();
+		combat.addMonsterToFight(m1);
+		Field monsterList = combat.getClass().getDeclaredField("monsters");
+		monsterList.setAccessible(true);
+		List<AbstractMonster> mL = (List<AbstractMonster>) monsterList.get(combat);
+		assertEquals(1,mL.size());	
+	}
+	
+	@Test
+	public void testCantAddMonsterTwice() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		AbstractMonster m1 = new AughostDerwraith();
+		combat.addMonsterToFight(m1);
+		combat.addMonsterToFight(m1);
+		Field monsterList = combat.getClass().getDeclaredField("monsters");
+		monsterList.setAccessible(true);
+		List<AbstractMonster> mL = (List<AbstractMonster>) monsterList.get(combat);
+		assertEquals(1,mL.size());	
+	}
+	
+	@Test
+	public void testAddPlayer() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Player p1 = new Player();
+		combat.addPlayerToFight(p1);
+		Field fighterList = combat.getClass().getDeclaredField("fighters");
+		fighterList.setAccessible(true);
+		List<Player> players = (List<Player>) fighterList.get(combat);
+		assertEquals(1,players.size());	
+	}
+	
+	@Test
+	public void testCantAddPlayerTwice() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException{
+		Player p1 = new Player();
+		combat.addPlayerToFight(p1);
+		combat.addPlayerToFight(p1);
+		Field fighterList = combat.getClass().getDeclaredField("fighters");
+		fighterList.setAccessible(true);
+		List<Player> players = (List<Player>) fighterList.get(combat);
+		assertEquals(1,players.size());	
+	}
+	
+	@Test
+	public void testPlayerCombatLevelSet() throws NoSuchFieldException, SecurityException, IllegalArgumentException, IllegalAccessException {
+		Player p1 = new Player();
+		p1.addLevel(5);
+		combat.addPlayerToFight(p1);
+		Field fighterList = combat.getClass().getDeclaredField("fighters");
+		fighterList.setAccessible(true);
+		List<Player> players = (List<Player>) fighterList.get(combat);
+		assertEquals(5,players.get(0).getCombatLevel());
+	}
+
+
+
+
+}
