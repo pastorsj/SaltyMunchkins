833e455c853c8778f8404f921a3c5f9b6db4ad96
Trent Punt <tpunt3@gmail.com>
2016-04-13 14:55:45 -0400

    fix to aughost derwraith and removing read file from main

--- src/munchkin/cards/doors/AughostDerwraith.java
+++ src/munchkin/cards/doors/AughostDerwraith.java
@@ -13,14 +13,16 @@ public class AughostDerwraith extends AbstractMonster {
 	public void cardInPlay() {
 		this.setTreasures(1);
 		this.setLevel(2);
-		//If owner is the only munchkin fighting the monster
-		if(this.getOwner().getFaction().equals(Faction.MonsterWhacker)) {
+		// If owner is the only munchkin fighting the monster
+		if (this.getOwner().getFaction().equals(Faction.MonsterWhacker)) {
 			this.setLevel(5);
 		}
 	}
 
 	@Override
 	public void badStuff() {
-		//If the owner was the only one fighting the monster and lost, he loses all of his classes
+		if (this.getOwner().getFaction() != Faction.Cultist) {
+			this.getOwner().setFaction(Faction.UNAFFILIATED);
+		}
 	}
 }
--- src/munchkin/game/Main.java
+++ src/munchkin/game/Main.java
@@ -10,8 +10,6 @@ public class Main {
 	}
 
 	public static void initMain() throws IOException {
-
-		ReadFile file1 = new ReadFile("resources/CardList.txt");
 		new MFrame(new Game(2));
 	}
 
