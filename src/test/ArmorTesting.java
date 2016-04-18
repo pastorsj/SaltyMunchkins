d6fb1da1359b0b8c4cea850d05c220ee66d26370
Trent Punt <tpunt3@gmail.com>
2016-04-18 15:09:33 -0400

    added most of armor testing

---
+++ src/test/ArmorTesting.java
@@ -0,0 +1,108 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import munchkin.cards.treasures.MinersHelmet;
+import munchkin.cards.treasures.MiskatonicUniversityTShirt;
+import munchkin.cards.treasures.MonsterStompers;
+import munchkin.cards.treasures.api.ArmorSet;
+import munchkin.cards.treasures.api.ITreasure;
+import munchkin.game.Action;
+import munchkin.game.Game;
+
+public class ArmorTesting {
+
+	private Action action;
+	private ArmorSet armor;
+	private Game game;
+	
+	@Before
+	public void setUp(){
+		action = Action.getInstance();
+		armor = new ArmorSet();
+		game = new Game(2);
+	}
+	
+	@Test
+	public void testAddFootGear() {
+		armor.removeFootgear();
+		
+		ITreasure notboots = new MinersHelmet();
+		notboots.cardInPlay();
+		ITreasure boots = new MonsterStompers(game);
+		armor.addFootGear(notboots);
+		assertEquals("Not Footgear armor",action.getAction());
+		assertEquals(0, armor.getFootgear().size());
+		
+		boots.cardInPlay();
+		armor.addFootGear(boots);
+		assertEquals(1, armor.getFootgear().size());
+		assertEquals(boots, armor.getFootgear().get(0));
+		
+		armor.addFootGear(boots);
+		assertEquals("Already at max armor for footgear", action.getAction());
+		assertEquals(1, armor.getFootgear().size());
+		
+		armor.removeFootgear();
+		armor.addFootGear(boots);
+		assertEquals(1, armor.getFootgear().size());
+		assertEquals(boots, armor.getFootgear().get(0));
+	}
+	
+	@Test
+	public void testAddHeadGear(){
+		armor.removeHeadgear();
+		
+		ITreasure headGear = new MinersHelmet();
+		ITreasure notHeadGear = new MonsterStompers(game);
+		notHeadGear.cardInPlay();
+		
+		armor.addHeadGear(notHeadGear);
+		assertEquals("Not HeadGear armor",action.getAction());
+		assertEquals(0, armor.getHeadgear().size());
+		
+		headGear.cardInPlay();
+		armor.addHeadGear(headGear);
+		assertEquals(1, armor.getHeadgear().size());
+		assertEquals(headGear, armor.getHeadgear().get(0));
+		
+		armor.addHeadGear(headGear);
+		assertEquals("Already at max armor for headgear", action.getAction());
+		assertEquals(1, armor.getHeadgear().size());
+		
+		armor.removeHeadgear();
+		armor.addHeadGear(headGear);
+		assertEquals(1, armor.getHeadgear().size());
+		assertEquals(headGear, armor.getHeadgear().get(0));
+	}
+	
+	@Test
+	public void testAddArmor(){
+		
+		ITreasure chest = new MiskatonicUniversityTShirt(game);
+		ITreasure notArmor = new MinersHelmet();
+		notArmor.cardInPlay();
+		
+		armor.addArmor(notArmor);
+		assertEquals("Not armor type",action.getAction());
+		assertEquals(0, armor.getArmor().size());
+		
+		chest.cardInPlay();
+		armor.addArmor(chest);
+		assertEquals(1, armor.getArmor().size());
+		assertEquals(chest, armor.getArmor().get(0));
+		
+		armor.addArmor(chest);
+		assertEquals("Already at max armor for armor", action.getAction());
+		assertEquals(1, armor.getArmor().size());
+		
+		armor.removeArmor();
+		armor.addArmor(chest);
+		assertEquals(1, armor.getArmor().size());
+		assertEquals(chest, armor.getArmor().get(0));
+		
+	}
+}
