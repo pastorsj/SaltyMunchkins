627a8b5ebe2491274151dd86928a6e6cbbf44f86
Sam Pastoriza <pastorsj@rose-hulman.edu>
2016-04-18 09:23:05 -0400

    And another try again and again and again and again and again

--- src/munchkin/game/panels/MainCardPanel.java
+++ src/munchkin/game/panels/MainCardPanel.java
@@ -13,7 +13,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
