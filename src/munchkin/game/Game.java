b2e66f52a834ecc11874012b6f5daf3f61e63f3d
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-04-20 20:43:51 -0400

    game refactored

--- src/munchkin/game/Game.java
+++ src/munchkin/game/Game.java
@@ -2,50 +2,37 @@ package munchkin.game;
 
 import munchkin.api.*;
 import munchkin.cards.doors.api.AbstractMonster;
-import munchkin.game.panels.MainCardPanel;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 import java.util.*;
 
 public class Game {
 
-	//Observed Value (DO NOT DELETE for now...)
+	// Observed Value (DO NOT DELETE for now...)
 	private Action action = Action.getInstance();
 	private static final int WIN_LEVEL = 10;
 	public static boolean gameSetUp = false;
 
-	//-----Potential New Implementation-----
-	//Assume only two players
+	// -----Potential New Implementation-----
+	// Assume only two players
 	private Queue<IPlayer> players;
-	//IMPORTANT: The concept of the hand in play
+	// IMPORTANT: The concept of the hand in play
 	private CardsInPlay cardsInPlay;
-	private List<ICard> doorCards;
-	private List<ICard> treasureCards;
-	private List<ICard> allCards;
-	private List<ICard> discardedDoorCards;
-	private List<ICard> discardedTreasureCards;
+	private InitializeCards initializeCards;
+	private DiscardedCards discardedCards;
 	private Combat combat;
 
-
 	public Game(int numberOfPlayers) {
-
-		InitializeCards initializeCards = new InitializeCards(this);
-		this.doorCards = initializeCards.getDoorCards();
-		this.treasureCards = initializeCards.getTreasureCards();
-		this.allCards = initializeCards.getAllCards();
-		this.discardedDoorCards = new ArrayList<>();
-		this.discardedTreasureCards = new ArrayList<>();
+		this.initializeCards = new InitializeCards(this);
+		this.discardedCards = new DiscardedCards();
 		this.cardsInPlay = new CardsInPlay(this);
 
 		this.players = new LinkedList<>();
-		for(int i = 0; i < numberOfPlayers; i++) {
+		for (int i = 0; i < numberOfPlayers; i++) {
 			this.players.add(new Player());
 		}
 		this.dealInitialCards();
 
-		//Constructs the idea of Combat, which will always have an referenced instance of the game
+		// Constructs the idea of Combat, which will always have an referenced
+		// instance of the game
 		this.combat = new Combat(this);
 
 	}
@@ -53,10 +40,10 @@ public class Game {
 	public IPlayer getCurrentPlayer() {
 		return this.players.peek();
 	}
-	
+
 	public IPlayer getOtherPlayer() {
 		IPlayer first = this.players.poll();
-		IPlayer second  = this.players.poll();
+		IPlayer second = this.players.poll();
 		this.players.offer(first);
 		this.players.offer(second);
 		return second;
@@ -83,31 +70,31 @@ public class Game {
 	}
 
 	public void dealInitialCards() {
-		for(IPlayer player : this.players) {
-			for(int i = 0; i < 4; i++) {
-				player.getHand().insertCard(this.doorCards.remove(0));
-				player.getHand().insertCard(this.treasureCards.remove(0));
+		for (IPlayer player : this.players) {
+			for (int i = 0; i < 4; i++) {
+				player.addCardToHand(this.initializeCards.removeDoorCard(0));
+				player.addCardToHand(this.initializeCards.removeTreasureCard(0));
 			}
 		}
 	}
 
-	public ICard dealNewCard(String cardPileType, IPlayer p) throws Exception {
-		List<ICard> cards;
-		ICard card;
-		if(cardPileType.equals("Door")) {
-			cards = this.doorCards;
-		} else if(cardPileType.equals("Treasure")) {
-			cards = this.treasureCards;
-		} else {
-			throw new Exception("No such card pile type. Must be either treasure or door");
-		}
-		if(cards.size() < 1) {
+	public ICard dealNewDoorCard(IPlayer p) {
+		return this.drawACard(this.initializeCards.getDoorCards(), p);
+	}
+
+	public ICard dealNewTreasureCard(IPlayer p) {
+		return this.drawACard(this.initializeCards.getTreasureCards(), p);
+	}
+
+	public ICard drawACard(List<ICard> cards, IPlayer p) {
+		if (cards.size() < 1) {
 			action.setValue("No cards left to draw");
-			//FIXME: Eventually, we want to just reshuffle the discarded piles and continue the game
+			// FIXME: Eventually, we want to just reshuffle the discarded piles
+			// and continue the game
 			return null;
 		} else {
-			card = cards.get(0);
-			if(p.getHand().insertCard(cards.get(0))) {
+			ICard card = cards.get(0);
+			if (p.getHand().insertCard(card)) {
 				return card;
 			} else {
 				action.setValue("You can only have eight cards in your hand. Please discard any extras");
@@ -115,29 +102,20 @@ public class Game {
 			}
 		}
 	}
-	
+
 	public boolean discardCard(IPlayer p, ICard card) throws Exception {
-		if(p.getHand().removeCardFromHand(card)) {
+		if (p.getHand().removeCardFromHand(card)) {
 			card.setOwner(null);
-			if (card.getCardType().equals(CardType.Door)) {
-				this.discardedDoorCards.add(card);
-				return true;
-			} else if (card.getCardType().equals(CardType.Treasure)) {
-				this.discardedTreasureCards.add(card);
-				return true;
-			} else {
-				System.err.println("This card has no type!");
-				return false;
-			}
+			return this.discardedCards.addToDiscardedCards(card);
 		}
 		return false;
 	}
 
 	public boolean playACard(ICard card) {
-		if(this.getCurrentPlayer().getArmorSet().checkArmor(card) && !card.isDisabled()) {
+		if (this.getCurrentPlayer().getArmorSet().checkArmor(card) && !card.isDisabled()) {
 			this.getCurrentPlayer().getHand().removeCardFromHand(card);
 			this.cardsInPlay.addCardsToPlay(card);
-			if(card instanceof AbstractMonster) {
+			if (card instanceof AbstractMonster) {
 				this.action.setValue("Added " + card.getName() + " monster to combat");
 				this.combat.addMonsterToFight((AbstractMonster) card);
 			}
@@ -149,12 +127,12 @@ public class Game {
 	}
 
 	public boolean endTurn() {
-		//TODO: Several things must happen when ending a turn
+		// TODO: Several things must happen when ending a turn
 		// Resolve conflict
 		this.combat.resolveFight();
 		// Discard all changes that are set to be discarded
-		for(ICard c : this.cardsInPlay.getCardsInPlay()) {
-			if(c.checkDiscard()) {
+		for (ICard c : this.cardsInPlay.getCardsInPlay()) {
+			if (c.checkDiscard()) {
 				this.cardsInPlay.removeCardFromPlay(c);
 			}
 		}
@@ -171,12 +149,12 @@ public class Game {
 	public Combat getCombat() {
 		return this.combat;
 	}
-	
+
 	public CardsInPlay getCardsInPlay() {
 		return this.cardsInPlay;
 	}
-	
+
 	public List<ICard> getAllCards() {
-		return this.allCards;
+		return this.initializeCards.getAllCards();
 	}
 }
