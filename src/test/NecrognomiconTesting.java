80c68948f5488329ae94b27e6ab137766cd487f0
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-11 10:10:57 -0400

    add test for Necronomicon

--- src/test/NecrognomiconTesting.java
+++ src/test/NecrognomiconTesting.java
@@ -1,26 +1,52 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.treasures.Necrognomicon;
+import munchkin.cards.treasures.api.Armor;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class NecrognomiconTesting {
 
-    @Before
-    public void setUp() {
+	private Necrognomicon card;
+	private Game game;
 
-    }
+	@Before
+	public void setUp() {
+		this.game = new Game(2);
+		this.card = new Necrognomicon(this.game);
+		this.card.setOwner(this.game.getCurrentPlayer());
+	}
 
-    @After
-    public void takeDown() {
+	@After
+	public void takeDown() {
+		this.game = null;
+		this.card = null;
+	}
 
-    }
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+		assertNotNull(this.game);
+	}
 
-    @Test
-    public void testInit() {
+	@Test
+	public void testCardInHand() {
+		this.card.cardInHand();
+		assertEquals(200, this.card.getGold());
+		assertEquals(Armor.OneHand, this.card.getArmor());
+	}
 
-    }
+	@Test
+	public void testCardInPlay() {
+		this.card.cardInPlay();
+		assertEquals(2, this.card.getBonus());
+	}
 }
