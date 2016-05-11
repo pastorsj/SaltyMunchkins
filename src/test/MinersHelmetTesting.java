005e72be5359f2ae614af6353e83623720d66700
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-11 10:44:59 -0400

    add test for MinersHelmet

--- src/test/MinersHelmetTesting.java
+++ src/test/MinersHelmetTesting.java
@@ -1,26 +1,46 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.treasures.MinersHelmet;
+import munchkin.cards.treasures.api.Armor;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class MinersHelmetTesting {
+	private MinersHelmet card;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
+		this.card = new MinersHelmet();
+	}
 
-    }
+	@After
+	public void takeDown() {
+		this.card = null;
+	}
 
-    @After
-    public void takeDown() {
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+	}
 
-    }
+	@Test
+	public void testCardInHandWithCultist() {
+		this.card.cardInHand();
+		assertEquals(400, this.card.getGold());
+		assertEquals(Armor.HeadGear, this.card.getArmor());
+	}
 
-    @Test
-    public void testInit() {
-
-    }
+	@Test
+	public void testCarInPlay() {
+		this.card.cardInPlay();
+		assertEquals(2, this.card.getBonus());
+	}
 }
