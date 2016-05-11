2c534f569090062e63544bf0c470d7b9e12c071e
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-10 21:08:10 -0400

    add test for SuddenHeadBlow

--- src/test/SuddenHeadBlowTesting.java
+++ src/test/SuddenHeadBlowTesting.java
@@ -1,26 +1,48 @@
 package test;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.doors.SuddenHeadBlow;
+import munchkin.cards.treasures.api.Faction;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class SuddenHeadBlowTesting {
+	private SuddenHeadBlow card;
+	private Game game;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
+		this.game = new Game(2);
+		this.card = new SuddenHeadBlow(this.game);
+		this.card.setOwner(this.game.getCurrentPlayer());
+		this.game.getOtherPlayer().setFaction(Faction.Cultist);
+	}
 
-    }
+	@After
+	public void takeDown() {
+		this.card = null;
+		this.game = null;
+	}
 
-    @After
-    public void takeDown() {
-
-    }
-
-    @Test
-    public void testInit() {
-
-    }
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+		assertNotNull(this.game);
+	}
+	
+	@Test
+	public void testCardInPlay(){
+		this.card.cardInPlay();
+		assertEquals(Faction.UNAFFILIATED, this.game.getOtherPlayer().getFaction());
+		
+		this.card.cardInPlay();
+		assertEquals(Faction.Cultist, this.game.getOtherPlayer().getFaction());
+	}
 }
