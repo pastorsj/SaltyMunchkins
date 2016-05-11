7cbe09968bc4d9b17fabaaf3e5cc3a5b78925ebd
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-11 10:23:52 -0400

    add test fro MonsterStompers

--- src/test/MonsterStompersTesting.java
+++ src/test/MonsterStompersTesting.java
@@ -1,26 +1,61 @@
 package test;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.treasures.MonsterStompers;
+import munchkin.cards.treasures.api.Armor;
+import munchkin.cards.treasures.api.Faction;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class MonsterStompersTesting {
 
-    @Before
-    public void setUp() {
+	private MonsterStompers card;
+	private Game game;
 
-    }
+	@Before
+	public void setUp() {
+		this.game = new Game(2);
+		this.card = new MonsterStompers(this.game);
+		this.card.setOwner(this.game.getCurrentPlayer());
+	}
 
-    @After
-    public void takeDown() {
+	@After
+	public void takeDown() {
+		this.game = null;
+		this.card = null;
+	}
 
-    }
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+		assertNotNull(this.game);
+	}
 
-    @Test
-    public void testInit() {
-
-    }
+	@Test
+	public void testCardInHandWithMonsterWhacker() {
+		this.card.getOwner().setFaction(Faction.MonsterWhacker);
+		this.card.cardInHand();
+		assertEquals(600, this.card.getGold());
+		assertEquals(Armor.FootGear, this.card.getArmor());
+		assertFalse(this.card.isDisabled());
+	}
+	@Test
+	public void testCardInHandWithoutMonsterWhacker() {
+		this.card.cardInHand();
+		assertEquals(600, this.card.getGold());
+		assertEquals(Armor.FootGear, this.card.getArmor());
+		assertTrue(this.card.isDisabled());
+	}
+	@Test
+	public void testCardInPlay() {
+		this.card.cardInPlay();
+		assertEquals(3, this.card.getBonus());
+	}
 }
