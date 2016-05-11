e2a2dd6fe8afad8cb4e24af0b7b4c46c12497513
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-10 21:19:19 -0400

    add test for Squamous

--- src/test/SquamousTesting.java
+++ src/test/SquamousTesting.java
@@ -1,26 +1,55 @@
 package test;
 
+import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.doors.Squamous;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class SquamousTesting {
+	private Squamous card;
+	private Game game;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
+		this.game = new Game(2);
+		this.card = new Squamous(this.game);
+	}
 
-    }
+	@After
+	public void takeDown() {
+		this.game = null;
+		this.card = null;
+	}
 
-    @After
-    public void takeDown() {
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+		assertNotNull(this.game);
+	}
 
-    }
+	@Test
+	public void testCardInPlay() {
+		this.card.cardInPlay();
+		assertEquals(5, this.card.getLevel());
+	}
 
-    @Test
-    public void testInit() {
+	@Test
+	public void testCardPlayedWin() {
+		this.card.cardPlayed(true);
+		assertEquals(1, this.card.getTreasures());
+	}
 
-    }
+	@Test
+	public void testCardPlayedLose() {
+		this.card.cardPlayed(false);
+		assertEquals(0, this.card.getTreasures());
+
+	}
 }
