68410c1cac38e4da7ecd941544b7487d8bd9f45e
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-11 10:42:32 -0400

    add test MiskatonicUniversityTShirt

--- src/test/MiskatonicUniversityTShirtTesting.java
+++ src/test/MiskatonicUniversityTShirtTesting.java
@@ -1,26 +1,51 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.treasures.MiskatonicUniversityTShirt;
+import munchkin.cards.treasures.api.Armor;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class MiskatonicUniversityTShirtTesting {
+	private MiskatonicUniversityTShirt card;
+	private Game game;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
+		this.game = new Game(2);
+		this.card = new MiskatonicUniversityTShirt(this.game);
+		this.card.setOwner(this.game.getCurrentPlayer());
+	}
 
-    }
+	@After
+	public void takeDown() {
+		this.game = null;
+		this.card = null;
+	}
 
-    @After
-    public void takeDown() {
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+		assertNotNull(this.game);
+	}
 
-    }
+	@Test
+	public void testCardInHandWithCultist() {
+		this.card.cardInHand();
+		assertEquals(100, this.card.getGold());
+		assertEquals(Armor.PseudoArmor, this.card.getArmor());
+	}
 
-    @Test
-    public void testInit() {
-
-    }
+	@Test
+	public void testCarInPlay() {
+		this.card.cardInPlay();
+		assertEquals(1, this.card.getBonus());
+	}
 }
