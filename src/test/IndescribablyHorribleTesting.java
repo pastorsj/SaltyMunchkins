48065f86cc53c54fd063d328e32edb2343a320e6
Luwen Zhang <zhangl3@rose-hulman.edu>
2016-05-11 21:08:39 -0400

    add test for IndescribablyHorrible

--- src/test/IndescribablyHorribleTesting.java
+++ src/test/IndescribablyHorribleTesting.java
@@ -1,26 +1,49 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.cards.doors.IndescribablyHorrible;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class IndescribablyHorribleTesting {
+	private IndescribablyHorrible card;
 
-    @Before
-    public void setUp() {
+	@Before
+	public void setUp() {
+		this.card = new IndescribablyHorrible();
+	}
 
-    }
+	@After
+	public void takeDown() {
+		this.card = null;
+	}
 
-    @After
-    public void takeDown() {
+	@Test
+	public void testInit() {
+		assertNotNull(this.card);
+	}
 
-    }
+	@Test
+	public void testCardInPlay() {
+		this.card.cardInPlay();
+		assertEquals(10, this.card.getLevel());
+	}
 
-    @Test
-    public void testInit() {
-
-    }
+	@Test
+	public void testCardPlayedWin() {
+		this.card.cardPlayed(true);
+		assertEquals(2, this.card.getTreasures());
+	}
+	@Test
+	public void testCardPlayedLose() {
+		this.card.cardPlayed(false);
+		assertEquals(0, this.card.getTreasures());
+	}
 }
