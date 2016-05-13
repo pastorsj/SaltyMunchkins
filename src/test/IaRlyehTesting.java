1511566f970e6be69156c8ddb5fce90c5c4b59e8
harrislb <lexibharris@yahoo.com>
2016-05-12 22:37:50 -0400

    IaRlyeh Testing

--- src/test/IaRlyehTesting.java
+++ src/test/IaRlyehTesting.java
@@ -1,26 +1,58 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.api.ICard;
+import munchkin.api.Player;
+import munchkin.cards.doors.Buggoth;
+import munchkin.cards.doors.IaRlyeh;
+import munchkin.cards.doors.api.Door;
+import munchkin.cards.treasures.api.Faction;
+import munchkin.game.Game;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class IaRlyehTesting {
+	
+	private Door door;
+	private Game game;
 
     @Before
     public void setUp() {
-
+    	game = new Game(2);
+    	door = new IaRlyeh(game);
+    
     }
 
     @After
     public void takeDown() {
-
+    	this.door = null;
     }
 
     @Test
-    public void testInit() {
-
+    public void testUse() {
+    	Player p = new Player();
+    	p.setFaction(Faction.MonsterWhacker);
+    	Door card = new Buggoth();
+    	card.setOwner(p);
+    	door.setOwner(p);
+    	card.cardInHand();
+    	door.cardInHand();
+    	card.cardInPlay();
+    	assertEquals(0, game.getDiscardedCards().getAllCards().size());
+    	
+    	game.getDiscardedCards().addToDiscardedCards(card);
+    	door.cardInPlay();
+    	
+    	assertTrue(p.getHand().getCards().contains(card));
+    	
     }
 }
--- src/test/InAMillionYearSleepTesting.java
+++ src/test/InAMillionYearSleepTesting.java
@@ -4,14 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import munchkin.cards.doors.InAMillionYearSleep;
+import munchkin.cards.doors.api.Door;
+import munchkin.game.Game;
+
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class InAMillionYearSleepTesting {
+	
+	private Door door;
+	private Game game;
 
     @Before
     public void setUp() {
-
+    	game = new Game(2);
+    	door = new InAMillionYearSleep(game);
+    	
+    	
+    	
     }
 
     @After
