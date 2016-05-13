be6098f14ee8550cc403f44505442c4235e89a21
harrislb <lexibharris@yahoo.com>
2016-05-12 20:07:29 -0400

    Grape Old Ones Testing

--- src/test/GrapeOldOnesTesting.java
+++ src/test/GrapeOldOnesTesting.java
@@ -1,26 +1,51 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import munchkin.api.Player;
+import munchkin.cards.doors.GrapeOldOnes;
+import munchkin.cards.doors.api.Door;
 
 /**
  * Created by SamPastoriza on 5/8/16.
  */
 public class GrapeOldOnesTesting {
+	
+	private Door door;
 
     @Before
     public void setUp() {
+    	door = new GrapeOldOnes();
+    	door.setOwner(new Player());
 
     }
 
     @After
     public void takeDown() {
+    	this.door = null;
 
     }
 
     @Test
-    public void testInit() {
-
+    public void testMale() {
+    	door.getOwner().setGender("Male");
+    	door.cardInHand();
+    	door.cardInPlay();
+    	assertEquals(12, door.getLevel());
+    	assertEquals(3, door.getTreasures());
+    	
+    }
+    
+    @Test
+    public void testFemale(){
+    	door.getOwner().setGender("Female");
+    	door.cardInHand();
+    	door.cardInPlay();
+    	assertEquals(10, door.getLevel());
+    	assertEquals(3, door.getTreasures());
     }
 }
